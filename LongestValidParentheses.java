5b47ab850732f5ca1028d4f078b2a6b9455f1130
Arun <jsarunchalam2005@gmail.com>
2026-07-20 17:58:49 +0530

    Implement longest valid parentheses function

---
+++ LongestValidParentheses.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class LongestValidParentheses {
+
+    public static int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+
+        // Base index to calculate valid substring length
+        stack.push(-1);
+
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLength = Math.max(maxLength, i - stack.peek());
+                }
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        System.out.println(longestValidParentheses(s));
+
+        sc.close();
+    }
+}
